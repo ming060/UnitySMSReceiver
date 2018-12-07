60c0cc9c28d6ca50e4704161cc75401ecd7b58d2
You-Ming <lym060@gmail.com>
2018-12-07 12:27:05 +0000

    initial commit

---
+++ UnityReceieveSMSDemo/Assets/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hello : MonoBehaviour {
+	public InputField OTPInputField;
+	void Start () {
+		UnitySMSReceiver.OnSMSReceiveHandler += OnSMSReceive;
+	}
+	private void OnSMSReceive (object o, UnitySMSReceiver.SMSReceiveEventArgs arg) {
+		Debug.Log (arg.Message);
+		OTPInputField.text = arg.Message;
+	}
+}
---
+++ UnityReceieveSMSDemo/Assets/Plugins/Android/UnitySMSReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitySMSReceiver : MonoBehaviour {
+	public class SMSReceiveEventArgs : EventArgs {
+		public String Message { get; private set; }
+		public SMSReceiveEventArgs (string message) {
+			Message = message;
+		}
+	}
+	private const string gameObjectName = "UnitySMSReceiver";
+	public static event EventHandler<SMSReceiveEventArgs> OnSMSReceiveHandler;
+	public void Awake () {
+		this.gameObject.name = gameObjectName;
+	}
+	public void OnSMSReceive (string message) {
+		OnSMSReceiveHandler (null, new SMSReceiveEventArgs (message));
+	}
+}
